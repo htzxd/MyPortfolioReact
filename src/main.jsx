dddc5292254d7d09fd886f8049a3fa83f3c7629d
marcope043@gmail.com <marcope043@gmail.com>
2024-07-31 15:43:42 -0300

    starting interface initial

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+import GlobalStyle from './styles/global'
+import theme from './styles/theme'
+import { ThemeProvider } from 'styled-components'
+
+import { Home } from './pages/home/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+export default {
+  colors: {
+    bg_main: "#000000ef",
+    bg_secondary: "#161616",
+    text_color: "#FFF",
+    title_color: "#9163a0cc",
+  }
+}
