5cdcb532885aac75296b5b876b4c23d25cb9c7fd
Marco Pereira <marcope043@gmail.com>
2024-09-09 15:04:22 -0300

    main jsx added

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
+import { StrictMode } from 'react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import GlobalStyle from './styles/global'
-import theme from './styles/theme'
-import { ThemeProvider } from 'styled-components'
-
-import { Routes } from './routes'
+import { App } from './app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Routes />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
