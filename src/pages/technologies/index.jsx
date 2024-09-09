914ed23040ba2b4c8e77be420fd98fcbe966cb08
Marco Pereira <marcope043@gmail.com>
2024-09-09 12:30:35 -0300

    port mvp ready

--- src/pages/technologies/index.jsx
+++ src/pages/technologies/index.jsx
@@ -13,27 +13,38 @@ export function Tech(){
   return (
     <Container>
       <Nav/>
+
+      <h1>Technologies</h1>
         <Main>
           <ul>
             <li>
               <img src={react} alt="tech logo" />
               <h2>React</h2>
+              <a href="#">Click here for seen my projects in React</a>
             </li>
+
             <li>
               <img src={js} alt="tech logo" />
               <h2>javascript</h2>
+              <a href="#">Click here for seen my projects in JavaScript</a>
             </li>
+
             <li>
               <img src={css} alt="tech logo" />
               <h2>css3</h2>
+              <a href="#">Click here for seen my projects in CSS3</a>
             </li>
+
             <li>
               <img src={html} alt="tech logo" />
               <h2>html5</h2>
+              <a href="#">Click here for seen my projects in HTML5</a>
             </li>
+
             <li>
               <img src={node} alt="tech logo" />
               <h2>nodejs</h2>
+              <a href="#">Click here for seen my projects in NodeJS</a>
             </li>
           </ul>
           
